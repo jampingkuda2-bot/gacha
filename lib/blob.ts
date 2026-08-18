aa6978a251092b0e78ae56cd6485355a74ae3423
AlexAlstr <jampingkuda2@gmail.com>
2026-08-18 14:25:05 +0700

    Add files via upload

--- lib/blob.ts
+++ lib/blob.ts
@@ -43,5 +43,6 @@ export async function saveConfig(config: GachaConfig): Promise<void> {
     access: "public",
     contentType: "application/json",
     addRandomSuffix: false,
+    cacheControlMaxAge: 0,
   });
 }
